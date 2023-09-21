d7e71a715e9bd92a3fd401e51a85dce5138d17ca
Tony P <kix99@protonmail.com>
2023-09-21 15:08:30 +0000

    map test

--- src/levelup/map.py
+++ src/levelup/map.py
@@ -1,4 +1,10 @@
 #map file
+from dataclasses import dataclass
 
 @dataclass
 
+class pirate_map
+    name = "Bermuda Triangle"
+
+    def __init__(self, map_name):
+        self.name = map_name
---
+++ src/levelup/map_test.y
@@ -0,0 +1,6 @@
+# Map test 
+from dataclasses import dataclass
+
+
+@dataclass
+class pirate_map_test
