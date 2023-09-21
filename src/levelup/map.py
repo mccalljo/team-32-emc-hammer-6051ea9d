826c02741152ae689dcc3206954ea428aa31e1e2
Tony P <kix99@protonmail.com>
2023-09-21 19:27:06 +0000

    ahoy mateys

--- src/levelup/map.py
+++ src/levelup/map.py
@@ -1,21 +1,10 @@
-#map file
+
 from dataclasses import dataclass
 
 @dataclass
-class pirate_map
+class pirate_map:
     name = "Bermuda Triangle"
 
     def __init__(self, map_name):
         self.name = map_name
 
-class pirate_map_cell:
-    x: int
-    y: int
-    terrain: str
-
-# Create a 10x10 map with default terrain "Water"
-map = [[pirate_map_cell(x, y, "Water") for x in range(10)] for y in range(10)]
-
-# Accessing a cell's terrain type (e.g., row 2, column 3)
-terrain_type = map[2][3].terrain
-print(f"Terrain at (2, 3): {terrain_type}")
---
+++ tests/test_map.py
@@ -0,0 +1,10 @@
+from unittest import TestCase
+from levelup.map import pirate_map
+
+"""
+class TestMap(TestCase):
+    def test_init(self):
+        ARBITRARY_POSITIONS = 100
+        testobj = GameMap(ARBITRARY_POSITIONS)
+        self.assertEqual(ARBITRARY_POSITIONS, testobj.numPositions)
+"""
