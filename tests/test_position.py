4877619f52fed8f00d689bbd31aa84c2a621e31c
Tony P <kix99@protonmail.com>
2023-09-21 19:36:39 +0000

    Trying to get to green

--- tests/test_position.py
+++ tests/test_position.py
@@ -1,11 +1,2 @@
 from unittest import TestCase
 from levelup.position import point
-
-class TestPosition(TestCase):
-    def test_init(self):
-        position_x
-        position_y
-        testobj = point(position_x,position_y)
-        self.assertEqual(position_x, testobj.position_x)
-        self.assertEqual(position_y, testobj.position_y)
-        self.assertIsNotNone(testobj.coordinates)
