1493a047f4309d1fca16f739520a27f8c829ddf4
Tony P <kix99@protonmail.com>
2023-09-21 19:33:48 +0000

    ugh

--- tests/test_position.py
+++ tests/test_position.py
@@ -9,19 +9,3 @@ class TestPosition(TestCase):
         self.assertEqual(position_x, testobj.position_x)
         self.assertEqual(position_y, testobj.position_y)
         self.assertIsNotNone(testobj.coordinates)
-
-    """
-    def test_maxpos(self):
-        position_x_max = 9
-        position_y_max = 9  
-        testObj = point()
-        self.assertEqual(position_x_max, testObj.position_x_max)
-        self.assertEqual(position_y_max, testObj.position_y_max)      
-
-    def test_minpos(self):
-        position_x_min = 0
-        position_y_min = 0          
-        testObj = point()
-        self.assertEqual(position_x_min, testObj.position_x_min)
-        self.assertEqual(position_y_min, testObj.position_y_min)  
-    """   
